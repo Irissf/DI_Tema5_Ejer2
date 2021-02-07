8972ad1f6c6c8def6ce0caf2f77e81bc4fc454df
Irissf <iris.seijo@gmail.com>
2021-02-07 18:13:09 +0100

    Agregar archivos de proyecto.

---
+++ DI_Tema5_Ejer2/EtiquetaAviso.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DI_Tema5_Ejer2
+{
+    public enum eMarca
+    {
+        Nada,
+        Cruz,
+        Circulo
+    }
+
+    public partial class EtiquetaAviso : Control
+    {
+        //cibermedium by Kent Nassen
+        /*
+        ____ ____ _  _ ____ ___ ____ _  _ ____ ___ ____ ____ 
+        |    |  | |\ | [__   |  |__/ |  | |     |  |  | |__/ 
+        |___ |__| | \| ___]  |  |  \ |__| |___  |  |__| |  \*/
+
+        public EtiquetaAviso()
+        {
+            InitializeComponent();
+        }
+
+        /*
+        ___  ____ ____ ___  _ ____ ___  ____ ___  ____ ____ 
+        |__] |__/ |  | |__] | |___ |  \ |__| |  \ |___ [__  
+        |    |  \ |__| |    | |___ |__/ |  | |__/ |___ ___]
+       */
+
+        //Marca==================================================
+        private eMarca marca = eMarca.Nada;
+        public eMarca Marca
+        {
+            set
+            {
+                marca = value;
+                this.Refresh();
+                // Lanzamos el Refresh para que se vuelva a pintar el componente al cambiar la marca.
+            }
+            get { return marca; }
+        }
+
+        /*
+        ____ _  _ ____ _  _ ___ ____ ____    ____ _  _ 
+        |___ |  | |___ |\ |  |  |  | [__     |  | |\ | 
+        |___  \/  |___ | \|  |  |__| ___]    |__| | \|
+         */
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics g = e.Graphics;
+            int grosor = 0; //Grosor de las líneas de dibujo
+            int offsetX = 0; //Desplazamiento a la derecha del texto
+            int offsetY = 0; //Desplazamiento hacia abajo del texto
+                             //Esta propiedad provoca mejoras en la apariencia o en la eficiencia
+                             // a la hora de dibujar
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            //Dependiendo del valor de la propiedad marca dibujamos una
+            //Cruz o un Círculo
+            switch (Marca)
+            {
+                case eMarca.Circulo:
+                    grosor = 20;
+                    g.DrawEllipse(new Pen(Color.Green, grosor), grosor, grosor,
+
+                    this.Font.Height, this.Font.Height);
+
+                    offsetX = this.Font.Height + grosor;
+                    offsetY = grosor;
+                    break;
+                case eMarca.Cruz:
+                    grosor = 5;
+                    Pen lapiz = new Pen(Color.Red, grosor);
+                    g.DrawLine(lapiz, grosor, grosor, this.Font.Height,
+                    this.Font.Height);
+                    g.DrawLine(lapiz, this.Font.Height, grosor, grosor,
+                    this.Font.Height);
+                    offsetX = this.Font.Height + grosor;
+                    offsetY = grosor / 2;
+                    //Es recomendable liberar recursos de dibujo pues se
+
+                    //pueden realizar muchos y cogen memoria
+
+                    lapiz.Dispose();
+                    break;
+            }
+            //Finalmente pintamos el Texto; desplazado si fuera necesario
+            SolidBrush b = new SolidBrush(this.ForeColor);
+            g.DrawString(this.Text, this.Font, b, offsetX + grosor, offsetY);
+            Size tam = g.MeasureString(this.Text, this.Font).ToSize();
+            this.Size = new Size(tam.Width + offsetX + grosor, tam.Height + offsetY
+            * 2);
+            b.Dispose();
+        }
+
+        protected override void OnTextChanged(EventArgs e)
+        {
+            base.OnTextChanged(e);
+            this.Refresh(); //cada vez que cambiamos el texto refrescamos los gráficos
+        }
+    }
+}
---
+++ PruebaComponente/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace PruebaComponente
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.etiquetaAviso1 = new DI_Tema5_Ejer2.EtiquetaAviso();
+            this.SuspendLayout();
+            // 
+            // etiquetaAviso1
+            // 
+            this.etiquetaAviso1.Location = new System.Drawing.Point(113, 142);
+            this.etiquetaAviso1.Marca = DI_Tema5_Ejer2.eMarca.Nada;
+            this.etiquetaAviso1.Name = "etiquetaAviso1";
+            this.etiquetaAviso1.Size = new System.Drawing.Size(78, 13);
+            this.etiquetaAviso1.TabIndex = 0;
+            this.etiquetaAviso1.Text = "etiquetaAviso1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.etiquetaAviso1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DI_Tema5_Ejer2.EtiquetaAviso etiquetaAviso1;
+    }
+}
+
